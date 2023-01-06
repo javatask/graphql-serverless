3bd62fc5e8945c33a3503519e69deb28a58d8191
Andrii Melashchenko <javatask@gmail.com>
2023-01-06 22:13:06 +0100

    Add random error to the resolver

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-Sample GraphQL serveless application build with AWS SAM
+# Sample GraphQL serveless application build with AWS SAM
+## Quick start
+```
+git clone https://github.com/javatask/graphql-serverless.git
+cd graphql-serverless
+npm i
+npm test
+npm run local-run
+```
--- src/resolvers/state.js
+++ src/resolvers/state.js
@@ -3,6 +3,10 @@ const text = process.env.TEST_VAR || "nothing";
 export const getState = () => {
   const state = `Hello ${text}`;
   console.log("State value:", state);
+  // throw error on random invocation
+  /*if (Math.random() > 0.5) {
+    throw new Error("This is an random error");
+  }*/
   return state;
 };
 
