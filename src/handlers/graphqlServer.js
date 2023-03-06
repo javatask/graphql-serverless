f912be2873f93c6e9bba879880c7a8d134f10e00
Andrii Melashchenko <javatask@gmail.com>
2023-03-06 10:29:41 +0100

    Update dependencies

--- src/handlers/graphqlServer.js
+++ src/handlers/graphqlServer.js
@@ -1,5 +1,8 @@
 import { ApolloServer } from "@apollo/server";
-import { startServerAndCreateLambdaHandler } from "@as-integrations/aws-lambda";
+import {
+  startServerAndCreateLambdaHandler,
+  handlers,
+} from "@as-integrations/aws-lambda";
 
 import { typeDefs } from "../schema/state.js";
 import { resolvers } from "../resolvers/state.js";
@@ -9,4 +12,7 @@ const server = new ApolloServer({
   resolvers,
 });
 
-export const handler = startServerAndCreateLambdaHandler(server);
+export default startServerAndCreateLambdaHandler(
+  server,
+  handlers.createAPIGatewayProxyEventRequestHandler()
+);
